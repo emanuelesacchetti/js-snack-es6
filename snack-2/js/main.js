987205b08229a9a7ea8036945bb45b0de3a310dc
emanuele sacchetti <manusacco92@gmail.com>
2023-03-10 18:07:05 +0100

    terminato snack 2

---
+++ snack-2/js/main.js
@@ -0,0 +1,47 @@
+const squadre = [
+    {
+        'nome': 'Roma',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'Juventus',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'Inter',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'Milan',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'Napoli',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+];
+
+
+
+squadre.forEach(function(element){
+        element.punti_fatti = generatoreNumeroCasuale(0, 100);
+        element.falli_subiti = generatoreNumeroCasuale(0, 50);
+});
+console.log(squadre);
+
+const teamArray = [];
+
+squadre.forEach(function(element){
+    const {nome, falli_subiti} = element;
+    teamArray.push(nome + ' ' + falli_subiti);
+})
+console.log(teamArray);
+
+function generatoreNumeroCasuale(min, max){
+    return Math.floor(Math.random() * (max - min +1));
+}
