b5f0081d6db6212e20f6d594694db34a7cc7014f
emanuele sacchetti <manusacco92@gmail.com>
2023-03-10 15:10:49 +0100

    prove dello snack 1. vari tentativi senza trovare soluzione

---
+++ snack-1/js/main.js
@@ -0,0 +1,48 @@
+
+const biciclette = [
+    {
+        'nome': 'bici1',
+        'peso': 5
+    },
+    {
+        'nome': 'bici2',
+        'peso': 7
+    },
+    {
+        'nome': 'bici3',
+        'peso': 8
+    },
+    {
+        'nome': 'bici4',
+        'peso': 6
+    }
+];
+
+
+const {nome, peso} = biciclette;
+console.log(nome, peso);
+
+let bici = [];
+biciclette.forEach(function(elemento, indice){
+    const {peso} = elemento;
+    
+    bici += peso
+    console.log(bici);
+});
+
+
+/*
+let bici = [];
+biciclette.forEach(function(elemento, indice){
+    bici.push(elemento['peso']);
+});
+
+console.log(bici);   //[5, 7, 8, 6]
+
+for ( let i = 0; i < bici.length; i++){
+    
+    if(bici[i] < bici[1]){
+        console.log(i)
+    }
+}
+*/
